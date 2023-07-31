2d3d6147275776efe72f891441b663d6b6ce0432
Luke Shay <38476395+lukeshay@users.noreply.github.com>
2023-07-31 16:13:54 -0500

    Update lukeshay-com.ts

--- src/configs/lukeshay-com.ts
+++ src/configs/lukeshay-com.ts
@@ -1,8 +1,6 @@
-import { PageRule, Zone } from "@pulumi/cloudflare";
+import { Zone } from "@pulumi/cloudflare";
 
-import { addRecords } from "../lib/deno-deploy";
-
-const zone = new Zone(
+new Zone(
 	"lukeshay-com",
 	{
 		plan: "free",
@@ -12,18 +10,3 @@ const zone = new Zone(
 		protect: true,
 	},
 );
-
-addRecords("fresh", zone.id, "8b4d7903a993d63d627962dc._acme.deno.dev");
-addRecords("www", zone.id, "7c9e8fcc6f7ea5fa0f62c238._acme.deno.dev");
-addRecords("@", zone.id, "dd2650195b4678fec5d9cae9._acme.deno.dev");
-
-new PageRule("lukeshay-com", {
-	actions: {
-		forwardingUrl: {
-			statusCode: 302,
-			url: "https://www.lukeshay.com/$1",
-		},
-	},
-	target: "https://lukeshay.com/*",
-	zoneId: zone.id,
-});
