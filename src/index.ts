b92e1a9ceb6a112460110d0d14b4a5845b532cfa
Luke Shay <shay.luke17@gmail.com>
2023-07-31 17:25:41 -0500

    comment out lukeshay-com

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "node:fs"
 
-export { outputs as lukeshayComOutputs } from "./configs/lukeshay-com"
+// export { outputs as lukeshayComOutputs } from "./configs/lukeshay-com"
 export { outputs as lshayCcOutputs } from "./configs/lshay-cc"
 export { outputs as lshayDevOutputs } from "./configs/lshay-dev"
 export { outputs as lshayLandOutputs } from "./configs/lshay-land"
